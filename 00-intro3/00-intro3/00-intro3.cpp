6680e5035fd8022b8295045d145c0e7f4b00408b
Лиза Путилова <102230201+lilpiv0@users.noreply.github.com>
2022-03-24 23:43:16 +0600

    00-intro3
    00-intro3

---
+++ 00-intro3/00-intro3/00-intro3.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	int a;
+	int b;
+	setlocale(LC_ALL, "Russian");
+	cout << "Введите переменную a \n";
+	cin >> a;
+	cout << "Введите переменную b \n";
+	cin >> b;
+	int res = pow((a + b), 2) + pow((a - b), 2) - (3 * a * b);
+	cout << "Результат выражения (a+b)^2+(a-b)^2-3ab = " << res;
+}
