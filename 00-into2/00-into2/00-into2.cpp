f34fbb070f21f2e64f53972cc0148d930dd55605
Лиза Путилова <102230201+lilpiv0@users.noreply.github.com>
2022-03-24 23:40:58 +0600

    00_intro2
    00_intro2

---
+++ 00-into2/00-into2/00-into2.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	int x;
+	setlocale(LC_ALL, "Russian");
+	cout << "Введите число, которые хотите ввести в квадрат \n";
+	cin >> x;
+	int y = pow(x, 2);
+	cout << "Результат: " << y;
+}
